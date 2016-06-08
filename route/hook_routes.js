ed742701da25558c3baa485c3c0bd66916c63f17
Dustin McBride <dustinbm@gmail.com>
2016-06-08 14:58:27 -0700

    fixed linting

--- route/hook_routes.js
+++ route/hook_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Alert = require('../model/alert');
 const User = require('../model/user');
 const hookRouter = module.exports = exports = express.Router();
-const twilio = require('../lib/twilio-client')
+const twilio = require('../lib/twilio-client');
 
 
 hookRouter.get('/firealert', (req, res, next) => {
@@ -17,7 +17,7 @@ hookRouter.get('/firealert', (req, res, next) => {
     }, (err, user) => {
       if (err) return next(err);
       console.log(user.phoneNumber);
-      twilio.sendSms(user.phoneNumber, "hello")
+      twilio.sendSms(user.phoneNumber, 'hello');
     });
   });
   next();
