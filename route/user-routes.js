bdd80fdee2391ecc8b8c77f2727570e4dcf63f81
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-08 09:47:25 -0700

    still working on user-routes

--- route/user-routes.js
+++ route/user-routes.js
@@ -1,27 +1,36 @@
 'use strict';
 
 const express = require('express');
-//const bodyParser = require('body-parser').json();
+// const bodyParser = require('body-parser').json();
 const User = require('../model/user');
+const jwtAuth = require('../lib/jwt');
 //const basicHTTP = require('../lib/basic_auth');
 // const superuser = require('../lib/super_auth.js');
 
-const router = module.exports = exports = express.Router();
+const userRouter = module.exports = exports = express.Router();
 
 
 //TODO GET /user/:id send user info
+userRouter.get('/user/:id', jwtAuth, (req, res, next) => {
+  User.findOne({_id: req.params.id}, (err, user) => {
+    console.log('not getting', user);
+    if(err || !user) return next(err);
+    res.json(user);
+  });
+});
+
 
 
 //TODO DELETE /user/:id delete user
 
 
 //TODO move to routes and create correct route name
-router.get('/:id/routes', (req, res, next) => {
-  User.findOne({_id: req.params.id}, (err, user) => {
-    if (err || !user) return next(new Error(err));
-    res.json(user.routes);
-  });
-});
+// router.get('/:id/routes', (req, res, next) => {
+//   User.findOne({_id: req.params.id}, (err, user) => {
+//     if (err || !user) return next(new Error(err));
+//     res.json(user.routes);
+//   });
+// });
 
 
 // TODO move to admin route
