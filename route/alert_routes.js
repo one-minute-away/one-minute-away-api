ab2263fa9a6a6bfb19e38088fc92eccecc9feda1
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-08 11:36:21 -0700

    reorganized alert routes, can post and get alert routes

--- route/alert_routes.js
+++ route/alert_routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser').json();
 const Alert = require('../model/alert');
 const jwtAuth = require('../lib/jwt');
-const User = require('../model/user');
 
 const alertRouter = module.exports = exports = express.Router();
 
@@ -16,43 +15,9 @@ alertRouter.post('/', bodyParser, jwtAuth, (req, res, next) => {
   });
 });
 
-// alertRouter.post('/', bodyParser, jwtAuth, (req, res, next) => {
-//   //let savedUser = req.user;
-//   let savedUserName = req.user.username;
-//   User.findOne({
-//     username: savedUserName
-//   })
-//     .exec((err, user) => {
-//       user.routes.push({
-//         nickname: req.body.nickname,
-//         route_id: req.body.route_id,
-//         stop_id: req.body.stop_id,
-//         hour: req.body.hour,
-//         minute: req.body.minute
-//       });
-//       user.save((err, user) => {
-//         if (err) return next(new Error(err));
-//         res.json(user);
-//       });
-//     });
-// });
-
-//TODO ensure only user can get a list of their own routes
 alertRouter.get('/', jwtAuth, (req, res, next) => {
-  Alert.find({}, (err, alerts) => {
+  Alert.find({userId:req.user._id}, (err, alerts) => {
     if (err) return next(err);
     res.json(alerts);
-  });
-});
-
-//TODO better error message
-alertRouter.use((req, res, next) => {
-  let cbString = req._parsedUrl.path.split('/').pop();
-  Alert.findOne({
-    cbString: cbString
-  }, (err, obj) => {
-    if (err) return next(err);
-    if (obj === null) return next(Error('bad bad bad'));
-    res.json(obj);
   });
 });
