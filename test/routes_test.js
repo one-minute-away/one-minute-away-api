080227cabf84d58655a8497ddad5201f65fde075
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-08 17:27:11 -0700

    fixed routes test post and get

---
+++ test/routes_test.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const User = require('../model/user');
+const mongoose = require('mongoose');
+chai.use(chaiHTTP);
+const expect = chai.expect;
+const request = chai.request;
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET || 'changeme';
+const dbPort = process.env.MONGOLAB_URI;
+
+process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
+
+require('../server');
+
+describe('User routes', () => {
+  let testUser;
+  let token;
+  beforeEach((done) => {
+    let newUser = new User({
+      username: 'testuser',
+      password: '$2a$08$pMewnngJdnSYxMz6dVcl8.H6PSiCqGCEP8Gri5zA6asB/qChSFMHq',
+      phoneNumber: '555555555',
+      email: 'test@test.com',
+      routes: []
+    });
+    newUser.save((err, user) => {
+      console.log(user);
+      testUser = user;
+      token = jwt.sign({
+        _id: testUser._id
+      }, secret);
+      done();
+    });
+  });
+  afterEach((done) => {
+    process.env.MONGOLAB_URI = dbPort;
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
+
+  it('should allow a user to access their saved routes', (done) => {
+    request('localhost:3000')
+      .get('/user/' + testUser._id + '/routes')
+      .set('token', token)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body).to.eql(testUser.routes);
+        done();
+      });
+  });
+
+  it('should allow a user to save a new route' , (done) => {
+    request('localhost:3000')
+    .post('/user/' + testUser._id + '/routes')
+    .set('token', token)
+    .send({
+      nickname: 'testNickName',
+      stop_id: 'testStop',
+      route_id: 'testId'
+    })
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      console.log('damn it jeff', testUser);
+      console.log(res.body.routes);
+      expect(res.body.routes[0].nickname).to.eql('testNickName');
+      done();
+    });
+  });
+
+describe('Route test that need routes already', () => {
+  before((done) => {
+  })
+    it('should allow a user to delete one of their saved        routes', (done) => {
+      request('localhost:3000')
+      .delete('/user/' + testUser._id + '/routes')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.eql('successfully deleted');
+        done();
+      });
+    });
+  });
+});
