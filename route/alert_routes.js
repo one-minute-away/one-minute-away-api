f3858561cb930cc6a234e61366528741ae4043c0
simonszc <zsimonson26@gmail.com>
2016-06-09 10:50:57 -0700

    added oba call to Alertsave callback

--- route/alert_routes.js
+++ route/alert_routes.js
@@ -5,6 +5,7 @@ const Alert = require('../model/alert');
 const jwtAuth = require('../lib/jwt');
 const getRoute = require('../lib/get-route');
 const getArrivals = require('../lib/get-arrivals');
+const http = require('http');
 
 const alertRouter = module.exports = exports = express.Router({mergeParams:true});
 
@@ -14,15 +15,37 @@ alertRouter.post('/', bodyParser, jwtAuth, getRoute, getArrivals, (req, res, nex
     userId: req.params.id,
     alertTimeOffset: timeOffset,
     routeId: req.route_id,
+    stopId: req.stop_id,
     tripId: req.tripId,
     stopSequence: req.stopSequence,
     serviceDate: req.serviceDate
   };
-  console.log(alertStuff);
   let newAlert = new Alert(alertStuff);
   newAlert.save((err, alert) => {
     if (err) return next(err);
-    res.json(alert);
+    alertStuff.id = alert._id;
+    let url = 'http://api.pugetsound.onebusaway.org/api/where/register-alarm-for-arrival-and-departure-at-stop/' +
+      alertStuff.stopId + '.json?' +
+      'key=' + process.env.OBA_KEY +
+      '&tripId=' + alertStuff.tripId +
+      '&serviceDate=' + alertStuff.serviceDate +
+      //'&vehicleId=' + alertObj.vehicleId +
+      '&stopSequence=' + alertStuff.stopSequence +
+      '&alarmTimeOffset=' + timeOffset +
+      '&url=' + process.env.CALLBACK_URL +
+      '&onArrival=true'
+      + '&alarm_id=' + alertStuff.id;
+      console.log(url);
+      http.get(url, (response, err) => {
+        var body = '';
+        response.on('data', function (chunk) {
+          body += chunk;
+        });
+        response.on('end', function () {
+          if(err) return next(new Error(err));
+          res.json(body);
+        });
+      });
   });
 });
 
